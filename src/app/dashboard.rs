2f0b017279690879507cf711bedd6d6a952d77e7
grantshandy <granthandy@proton.me>
2023-12-30 18:42:49 -0700

    update dashboard

--- src/app/dashboard.rs
+++ src/app/dashboard.rs
@@ -5,56 +5,69 @@ use crate::client;
 
 #[component]
 pub fn Dashboard() -> impl IntoView {
-    let client = create_resource(|| (), |_| async move {
-        client::get_current_user().await });
-    
-
     view! {
-        <div class="grow">
-            <Suspense>
-                {move || match client.get() {
-                    Some(Ok(Some(user))) => Some(view! { <User user=user/> }.into_view()),
-                    Some(Ok(None)) => {
-                        cfg_if::cfg_if! { if #[cfg(feature = "ssr")] {
-                            leptos_axum::redirect("/");
-                        } else {
-                            leptos_router::use_navigate()("/", Default::default());
-                        }}
-
-                        None
-                    },
-                    Some(Err(err)) => Some(view! { <p>"An Error " {err.to_string()}</p> }.into_view()),
-                    None => Some(view! { <p>"Loading View"</p> }.into_view()),
-                }}
-
-            </Suspense>
+        <div class="grow p-4">
+            <User />
         </div>
     }
 }
 
 #[component]
-fn User(
-    user: PrivateUser
-) -> impl IntoView {
-    let img_url = user.images.expect("no profile pictures").get(0).expect("no profile picture").url.clone();
+pub fn User() -> impl IntoView {
+    let client = create_resource(|| (), |_| async move {
+        client::get_current_user().await });
 
-    view! {
-        <div class="card w-96 bg-base-100 shadow-xl image-full">
-            <div class="card-body">
-                <h2 class="card-title">
-                    <div class="avatar">
-                        <div class="w-12 mask mask-squircle">
-                            <img src={img_url} />
-                        </div>
-                    </div>
-                    {user.display_name.unwrap_or("Unknown Username".to_string())}
-                </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+    // "skeleton" <- THIS IS A LOAD BEARING COMMENT. I SHIT YOU NOT.
+
+    let user_widget = |user: Option<PrivateUser>| view! {
+        <div class="mx-auto w-32 text-center rounded-xl p-2 shadow-xl bg-base-400">
+            <div class="avatar">
+                <div class="w-12 mask mask-squircle" class:skeleton=user.is_none()>
+                    {user
+                        .as_ref()
+                        .map(|user| {
+                            view! {
+                                <img src=user
+                                    .images
+                                    .clone()
+                                    .expect("no profile pictures")
+                                    .get(0)
+                                    .expect("no profile picture")
+                                    .url
+                                    .clone()/>
+                            }
+                        })}
                 </div>
             </div>
+            <p class="text-xl font-bold" class:skeleton=user.is_none()>
+                {user
+                    .map(|user| user.display_name)
+                    .flatten()
+                    .unwrap_or_default()}
+            </p>
         </div>
+    }.into_view();
+
+    view! {
+        <Suspense fallback=move || user_widget(None)>
+            {move || {
+                client
+                    .get()
+                    .map(|client| match client {
+                        Ok(Some(user)) => Some(user_widget(Some(user))),
+                        Ok(None) => {
+                            cfg_if::cfg_if! {
+                                if #[cfg(feature = "ssr")] { leptos_axum::redirect("/"); } else
+                                { leptos_router::use_navigate() ("/", Default::default()); }
+                            }
+                            None
+                        }
+                        Err(err) => {
+                            Some(view! { <p>"An Error " {err.to_string()}</p> }.into_view())
+                        }
+                    })
+            }}
+
+        </Suspense>
     }
 }
-
